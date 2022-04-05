b3843e2df3a1b88ff0d3b13583f6f9fcfa9f7aaa
knechtel <maiquelknechtel@gmail.com>
2022-04-05 14:14:19 -0300

    first commit

--- app/controllers/payload_controller.rb
+++ app/controllers/payload_controller.rb
@@ -1,6 +1,16 @@
 class PayloadController < ApplicationController
   skip_before_action :verify_authenticity_token
-  def payload
-    render json: {hello: 'world'}
+  def create
+  #  binding.pry
+    puts "Maiquel"
+    respond_to do |format|
+      format.json do
+        render :json => { 
+           :status => :ok, 
+           :message => "Success!",
+           :html => "<b>congrats</b>"
+        }.to_json
+       end 
+      end 
   end
 end
